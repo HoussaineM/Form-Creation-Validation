500cfca345e14a8c8a5b0d33219010dc77d858c8
HoussaineM <houssinemalme@hotmail.com>
2024-07-21 20:40:02 +0200

    Fetching Data from an API and Displaying It

--- fetch-data.js
+++ fetch-data.js
@@ -3,6 +3,7 @@ async function fetchUserData()
     const apiUrl = 'https://jsonplaceholder.typicode.com/users';
     const dataContainer = document.getElementById('api-data');
     
+    
     try {
         const response = await fetch(apiUrl);                    
         const users = await response.json();                     
